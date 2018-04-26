b5fe1854dcf264cd56d6e69f19c8e32ca5a31361
dengronglong <dengronglong@foxmail.com>
2018-04-26 18:15:44 +0800

    Spring Cloud Config

---
+++ microservice-config-client/src/main/java/com/jamin/microserviceconfigclient/MicroserviceConfigClientApplication.java
@@ -0,0 +1,17 @@
+package com.jamin.microserviceconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:
+ *
+ * @author 杰明Jamin
+ * @date 2018/4/26 17:59
+ */
+@SpringBootApplication
+public class MicroserviceConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConfigClientApplication.class, args);
+    }
+}
---
+++ microservice-config-client/src/main/java/com/jamin/microserviceconfigclient/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.jamin.microserviceconfigclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description:
+ *
+ * @author 杰明Jamin
+ * @date 2018/4/26 18:01
+ */
+@RestController
+public class ConfigClientController {
+
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return this.profile;
+    }
+
+}
